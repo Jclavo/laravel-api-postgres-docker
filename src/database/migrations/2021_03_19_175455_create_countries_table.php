fa81a46fb815d0933a7b8fafdf1c3dc294340eee
jClavo <jclavotafur@gmail.com>
2021-03-19 16:17:07 -0300

    test laravel sync with progress

--- src/database/migrations/2021_03_19_175455_create_countries_table.php
+++ src/database/migrations/2021_03_19_175455_create_countries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFlightsTable extends Migration
+class CreateCountriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateFlightsTable extends Migration
      */
     public function up()
     {
-        Schema::create('flights', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
-            ///
+            $table->string('name');
         });
     }
 
@@ -27,6 +26,6 @@ class CreateFlightsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('flights');
+        Schema::dropIfExists('countries');
     }
 }
